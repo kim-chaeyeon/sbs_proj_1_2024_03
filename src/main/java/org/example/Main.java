d5f5fb5250fdab50d3b22a85a7a72128daa9db5f
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-03-20 15:45:05 +0900

    게시물 작성기능 구현, 리스트 기능 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,75 @@
 package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 프로젝트 시작 ==");
+        System.out.println("== 프로그램 시작 ==");
         Scanner sc = new Scanner(System.in);
 
         int lastArticleId = 0;
 
-        while (true) {
-            System.out.printf("명령어)");
+        List<Article> articles = new ArrayList<>();
+
+        while ( true ) {
+            System.out.printf("명령어) ");
             String cmd = sc.nextLine();
-            cmd = cmd.trim(); // 양 옆 공백 제거
-            if (cmd.length() == 0) {
+            cmd = cmd.trim();
+
+            if ( cmd.length() == 0 ) {
                 continue;
             }
 
-            if (cmd.equals("exit")) {
+            if ( cmd.equals("exit") ) {
                 break;
             }
 
-            if (cmd.equals("article write")) {
+            if ( cmd.equals("article write") ) {
                 int id = lastArticleId + 1;
                 lastArticleId = id;
-                System.out.println("제목 : ");
+                System.out.printf("제목 : ");
                 String title = sc.nextLine();
-                System.out.println("내용 : ");
-                String boby = sc.nextLine();
+                System.out.printf("내용 : ");
+                String body = sc.nextLine();
+
+                Article article = new Article(id, title, body);
+                articles.add(article);
 
                 System.out.printf("%d번 글이 생성되었습니다.\n", id);
             }
-            else if (cmd.equals("article list")) {
-                System.out.println("게시물이 없습니다.");
+            else if ( cmd.equals("article list") ) {
+                if ( articles.size() == 0 ) {
+                    System.out.println("게시물이 없습니다.");
+                    continue;
+                }
+
+                System.out.println("번호 | 제목");
+                for ( int i = articles.size() - 1; i >= 0 ; i-- ) {
+                    Article article = articles.get(i);
+
+                    System.out.printf("%d    | %s\n", article.id, article.title);
+                }
             }
             else {
-                System.out.printf("%s(은)는 존재하지 않는 명령어 입니다\n", cmd);
+                System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", cmd);
             }
         }
-        // Scanner 닫기
+
         sc.close();
-        System.out.println("== 프로젝트 시작 ==");
+        System.out.println("== 프로그램 끝 ==");
+    }
+}
+
+class Article {
+    int id;
+    String title;
+    String body;
+
+    public Article(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
     }
 }
