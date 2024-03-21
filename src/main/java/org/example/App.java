f9337429871f17e1226850bfc9157f61d7c8669b
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-03-21 17:29:14 +0900

    게시물 리스트에 검색 기능 추가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -47,20 +47,41 @@ public class App {
 
                 System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
             }
-            else if ( cmd.equals("article list") ) {
+            else if ( cmd.startsWith("article list") ) {
                 if ( articles.size() == 0 ) {
                     System.out.println("게시물이 없습니다.");
                     continue;
                 }
 
+                String searchKeyword = cmd.substring("article list".length()).trim();
+
+                System.out.printf("검색어 : %s\n", searchKeyword);
+
+                List<Article> forListArticles = articles;
+
+                if (searchKeyword.length() > 0) {
+                    forListArticles = new ArrayList<>();
+
+                    for (Article article : articles) {
+                        if (article.title.contains(searchKeyword)) {
+                            forListArticles.add(article);
+                        }
+                    }
+
+                    if ( articles.size() == 0 ) {
+                        System.out.println("검색 결과가 존재하지 않습니다.");
+                        continue;
+                    }
+                }
+
                 System.out.println("번호 | 조회 | 제목 ");
-                for ( int i = articles.size() - 1; i >= 0 ; i-- ) {
-                    Article article = articles.get(i);
+                for ( int i = forListArticles.size() - 1; i >= 0 ; i-- ) {
+                    Article article = forListArticles.get(i);
 
                     System.out.printf("%4d | %4d | %s\n", article.id, article.hit, article.title);
                 }
             }
-            else if ( cmd.startsWith("article detail ") ) {
+            else if ( cmd.startsWith("article detail") ) {
                 String[] cmdBits = cmd.split(" ");
                 int id = Integer.parseInt(cmdBits[2]); // "1" => 1
 
