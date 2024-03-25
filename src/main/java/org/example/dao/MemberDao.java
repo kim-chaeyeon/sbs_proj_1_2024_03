966ac57d4bd07b9c4c9327af917e83df26fd798a
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-03-25 18:29:59 +0900

    회원 서비스 도입

--- src/main/java/org/example/dao/MemberDao.java
+++ src/main/java/org/example/dao/MemberDao.java
@@ -8,12 +8,35 @@ import java.util.List;
 public class MemberDao extends Dao {
     public List<Member> members;
 
-    public MemberDao () {
+    public MemberDao() {
         members = new ArrayList<>();
     }
 
-    public void add(Member member) {
+    public void join(Member member) {
         members.add(member);
         lastId++;
+    }
+
+    public int getMemberIndexByLoginId(String loginId) {
+        int i = 0;
+
+        for ( Member member : members ) {
+            if ( member.loginId.equals(loginId) ) {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+
+    public Member getMemberByLoginId(String loginId) {
+        int index = getMemberIndexByLoginId(loginId);
+
+        if ( index == -1 ) {
+            return null;
+        }
+
+        return members.get(index);
     }
 }
